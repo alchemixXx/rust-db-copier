5429dc671b303034adb13f4b6486e417d095a1b3
Pavlo Pavlenko <pavlo.pavlenko@edetek.com>
2024-03-04 10:11:37 +0200

    Add CLI argument for config path

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Refactoring plans
 
-- add ability to provide config path as CLI arg
+- add ability to provide config path as named CLI arg
 - add logger with levels and ability to enable/disable logging from config
 - add error handling (replace panic! with proper error)
 - rewrite logic to perfor tables migration not sequentially, but in batches
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use clap::Parser;
 mod config;
 mod traits;
 mod mysql_processor;
 mod error;
+mod cli;
+use cli::CLi;
 use error::CustomResult;
 use mysql_processor::migrator::Migrator;
 
 fn main() -> CustomResult<()> {
+    println!("Reading cli args...");
+    let cli_args = CLi::parse();
+    println!("CLI args: {:#?}", cli_args);
+
     println!("Reading script config");
     let config = config::read_config();
     println!("Read script config");
